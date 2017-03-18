fca03d4cd518cafffaf5c988ca2a35904f042275
Eddie Granados <egranados04@live.com>
2017-03-18 13:23:24 -0400

    Add files via upload

---
+++ If.java
@@ -0,0 +1,14 @@
+
+public class If {
+
+	public static void main(String[] args) {
+		if (7 > 7) {
+			
+			System.out.println("Try again...");
+		} else {
+			System.out.println("Great!");
+		}
+
+	}
+
+}
